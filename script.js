2ede394249f7d115feab1f4f95984ea9ef7c63cf
Giuliana <116002662+Emboiss13@users.noreply.github.com>
2025-09-14 15:48:39 +0100

    Implement tabbed component functionality and improve event delegation for tab clicks

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 //-------------------------------------------------
 // MODAL WINDOW
@@ -60,4 +64,32 @@ btnScrollTo.addEventListener('click', function () {
 
 btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//-------------------------------------------------
+// TABBED COMPONENT
+//-------------------------------------------------
+
+tabsContainer.addEventListener('click', function (e) {
+  //This is not useful as we also want to click on the span element not just the button
+  //const clicked = event.target; 
+
+  //What we want is to trigger the event regardless of us clicking on the parent (button) or child (span)
+  const clicked = e.target.closest('.operations__tab');
+
+  //Guard clause
+  if (!clicked) return;
+
+  //Remove active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(content => content.classList.remove('operations__content--active'));
+
+  //Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content area
+  //What we are doing here is changing the content based on the selerced tab
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
